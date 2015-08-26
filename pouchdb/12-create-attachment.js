33ad04bfe62d3c5c57753cc145f81fa14d2acf18
Bradley Holt <bradley.holt@us.ibm.com>
2015-08-26 18:01:59 -0400

    Fix attachment name

--- pouchdb/12-create-attachment.js
+++ pouchdb/12-create-attachment.js
@@ -1,8 +1,8 @@
 // Create an attachment
 var db = new PouchDB("smart-meter");
 var data = "R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=";
-db.putAttachment("myDoc", "pixel.png", data, "image/gif").then(function() {
-  return db.getAttachment("myDoc", "pixel.png");
+db.putAttachment("myDoc", "pixel.gif", data, "image/gif").then(function() {
+  return db.getAttachment("myDoc", "pixel.gif");
 }).then(function(attachment) {
   console.log(attachment);
   // Delete the database

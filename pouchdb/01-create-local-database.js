49c0fa37e3ee672de3bde9a3d2b382f87bc3e5e4
Bradley Holt <bradley.holt@us.ibm.com>
2015-11-24 15:24:22 -0500

    Fix example

--- pouchdb/01-create-local-database.js
+++ pouchdb/01-create-local-database.js
@@ -1,8 +1,9 @@
 // Create a local PouchDB database
 var db = new PouchDB("smart-meter");
+console.log("Database created");
 // Delete the database
 db.destroy().then(function() {
-  console.log("Database created");
+  console.log("Database destroyed");
 }).catch(function(error) {
   console.log(error);
 });
